b7919f840b3e8ac1402866803dca0682c06ebae4
Irina POGARTSEV <pogartseva.irina@gmail.com>
2022-09-27 09:25:43 +0200

    theme palette

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+        palette: {
+          // header color
+          primary: {
+            main: '#204B8E',
+          },
+          //footer color
+          secondary: {
+            main: '#1B3F6A',
+          },
+          // background 
+          dark: {
+            main: '#0C0C0C',
+          },
+          // text color
+          light: {
+            main: '#EBEBEB'
+          }
+        },
+      
+})
